1dea016fb4696459146df1aebee5a4b49742317a
Naoya Inoue <naoya.inoue.lab@gmail.com>
2022-03-07 21:45:31 -0500

    Configure mode.

--- src/spanbertcoref/configure.py
+++ src/spanbertcoref/configure.py
@@ -2,6 +2,9 @@ import importlib_resources
 import os
 
 
+RESOURCE_ROOT = importlib_resources.files("spanbertcoref")
+
+
 def compile_kernel():
     import tensorflow as tf
     tf_extra_flags = tf.sysconfig.get_compile_flags() + tf.sysconfig.get_link_flags()
@@ -9,13 +12,24 @@ def compile_kernel():
 
     print("Compiling kernel...")
     
-    cc_file = importlib_resources.files("spanbertcoref") / "lib" / "coref_kernels.cc"
-    so_file = importlib_resources.files("spanbertcoref") / "lib" / "coref_kernels.so"
+    cc_file = RESOURCE_ROOT / "lib" / "coref_kernels.cc"
+    so_file = RESOURCE_ROOT / "lib" / "coref_kernels.so"
 
     os.system(f"g++ -std=c++11 -shared {cc_file} -o {so_file} -fPIC {tf_extra_flags} -O2 -D_GLIBCXX_USE_CXX11_ABI=1")
 
 
+def maybe_copy_conf():
+    origin_conf_file = RESOURCE_ROOT / "template.conf"
+    conf_file = os.path.expanduser("~/.spanbertcoref.conf")
+
+    if not os.path.exists(conf_file):
+        print("Copying conf file...")
+        os.system(f"cp {origin_conf_file} {conf_file}")
+
+
 def main():
     compile_kernel()
+    maybe_copy_conf()
+
 
 main()
--- src/spanbertcoref/lib/coref_ops.py
+++ src/spanbertcoref/lib/coref_ops.py
@@ -1,8 +1,11 @@
 import importlib_resources
 import tensorflow as tf
-from tensorflow.python import pywrap_tensorflow
 
-coref_op_library = tf.load_op_library(importlib_resources.files("spanbertcoref") / "lib" / "coref_kernels.so")
+
+RESOURCE_ROOT = importlib_resources.files("spanbertcoref")
+
+
+coref_op_library = tf.load_op_library(RESOURCE_ROOT / "lib" / "coref_kernels.so")
 
 extract_spans = coref_op_library.extract_spans
 tf.NotDifferentiable("ExtractSpans")
