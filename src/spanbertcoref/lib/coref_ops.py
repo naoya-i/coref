6514db9284fc2a4370a928df01b4bfa772f8a6fd
Naoya Inoue <naoya.inoue.lab@gmail.com>
2022-03-07 21:55:50 -0500

    must be str.

--- src/spanbertcoref/lib/coref_ops.py
+++ src/spanbertcoref/lib/coref_ops.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 RESOURCE_ROOT = importlib_resources.files("spanbertcoref")
 
 
-coref_op_library = tf.load_op_library(RESOURCE_ROOT / "lib" / "coref_kernels.so")
+coref_op_library = tf.load_op_library(str(RESOURCE_ROOT / "lib" / "coref_kernels.so"))
 
 extract_spans = coref_op_library.extract_spans
 tf.NotDifferentiable("ExtractSpans")
